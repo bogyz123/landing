16b720e5750dc5cb8c2697ae65186e2db4cfc5b5
Bogdan <bogdandjakovic123@gmail.com>
2024-02-10 16:18:40 +0100

    add tilt

--- index.js
+++ index.js
@@ -1,67 +1,63 @@
-
 const scaleNav = (action) => {
-    const nav = document.getElementsByClassName("project-list")[0];
-    switch (action) {
-        case 'scale': {
-            nav.style.scale = '1.5'
-            break;
-        }
-        case 'descale': {
-            nav.style.scale = '1'
-            break;
-        }
+  const nav = document.getElementsByClassName("project-list")[0];
+  switch (action) {
+    case "scale": {
+      nav.style.scale = "1.5";
+      break;
     }
-}
+    case "descale": {
+      nav.style.scale = "1";
+      break;
+    }
+  }
+};
 const popProject = (popType, projects) => {
-    switch (popType) {
-        case 'pop': {
-            for (let i = 0; i < projects.length; i++) {
-                document.getElementById(projects[i]).style.scale = '1.6';
-                document.getElementById(projects[i]).style.padding = '0 30px';
-                document.getElementById(projects[i]).style.color = 'orange';
-            }
-            break;
-        }
-        case 'unpop': {
-            for (let i = 0; i < projects.length; i++) {
-                document.getElementById(projects[i]).style.scale = '1';
-                document.getElementById(projects[i]).style.padding = '0';
-                document.getElementById(projects[i]).style.color = 'white';
-            }
-            break;
-        }
-        default: {
-            break;
-        }
+  switch (popType) {
+    case "pop": {
+      for (let i = 0; i < projects.length; i++) {
+        document.getElementById(projects[i]).style.scale = "1.6";
+        document.getElementById(projects[i]).style.padding = "0 30px";
+        document.getElementById(projects[i]).style.color = "orange";
+      }
+      break;
     }
-}
+    case "unpop": {
+      for (let i = 0; i < projects.length; i++) {
+        document.getElementById(projects[i]).style.scale = "1";
+        document.getElementById(projects[i]).style.padding = "0";
+        document.getElementById(projects[i]).style.color = "white";
+      }
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+};
 var urlToOpen = null;
 const unpop = (project) => {
-    document.getElementById(project).style.scale = '1'
-    document.getElementById(project).style.margin = '0'
-}
+  document.getElementById(project).style.scale = "1";
+  document.getElementById(project).style.margin = "0";
+};
 const openUrl = (url) => {
-    document.getElementById("link-warning").style.display = 'flex';
-    document.getElementById("link-url").textContent = 'this will open ' + url;
-    urlToOpen = url;
-}
+  document.getElementById("link-warning").style.display = "flex";
+  document.getElementById("link-url").textContent = "this will open " + url;
+  urlToOpen = url;
+};
 const handleLink = (status) => {
-    if (status === 'failure') {
-        document.getElementById("link-warning").style.display = 'none';
-        return;
-    }
-    else {
-        document.getElementById("link-warning").style.display = 'none';
-        window.open(urlToOpen, "_blank");
-    }
-}
+  if (status === "failure") {
+    document.getElementById("link-warning").style.display = "none";
+    return;
+  } else {
+    document.getElementById("link-warning").style.display = "none";
+    window.open(urlToOpen, "_blank");
+  }
+};
 const handleMenu = () => {
-    const menu = document.getElementById("menu");
-    console.log(menu.style.display);
-    if (menu.style.display === 'none' || menu.style.display === '') {
-        menu.style.display = 'block';
-    }
-    else {
-        menu.style.display = 'none';
-    }
-}
+  const menu = document.getElementById("menu");
+  if (menu.style.display === "none" || menu.style.display === "") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+};
